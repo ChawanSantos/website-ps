f1ff658cfc5e85036b35d33ce3c8ce24042fbec8
Chawan <chawandossantos7@gmail.com>
2024-04-15 20:29:12 -0300

    alteração na pag home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,31 @@
+import './style.css'
+
+
+import fotoNoticia1 from '../../assets/noticia1.jpg'
+import fotoAntiga from '../../assets/praiasol-old.jpg'
+
+
 export default function Home() {
     return (
         <>
-            <h1>Bem vindo a página incial</h1>
+            <div className='container-topo'>
+                <img src={fotoNoticia1} alt="Foto noticia 1" />
+            </div>
+
+
+            <div className='sobre'>
+                <h2>SOBRE A VIAÇÃO PRAIA SOL</h2>
+                <h3>CONHEÇA A VIAÇÃO<br/>PRAIA SOL</h3>
+                <img src={fotoAntiga} alt="foto antiga" />
+
+
+                <div className='conteudo-historia-ps'>
+                    <p>A VIAÇÃO PRAIA SOL NASCEU DA VIAÇÃO ALVORADA, QUANDO ESTA QUE OPERAVA NO TRANSPORTE RODOVIÁRIO DE PASSAGEIROS, DECIDIU OPERAR TAMBÉM NO TRANSPORTE COLETIVO INTERMUNICIPAL DA GRANDE VITÓRIA.  ASSIM, FOI CRIADA A VIAÇÃO PRAIA SOL, VOLTADA INTEIRAMENTE AO TRANSPORTE COLETIVO INTERMUNICIPAL DE PASSAGEIROS EM 01 DE JANEIRO 1989.
+                    <br /><br />
+                    TRABALHAMOS PARA OFERECER SEGURANÇA, CONFORTO, PONTUALIDADE E CORDIALIDADE NO TRANSPORTE COLETIVO DE PASSAGEIROS
+                    </p>
+                </div>
+            </div>
         </>
     )
 }
